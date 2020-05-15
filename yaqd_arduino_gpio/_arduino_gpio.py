73511da41e93e3108ba7414e24fd3704d377c300
Brandon Mehlenbacher <bmehlenbacher4@gmail.com>
2020-05-15 08:24:51 -0500

    pyfirmata implementation

--- yaqd_arduino_gpio/_arduino_gpio.py
+++ yaqd_arduino_gpio/_arduino_gpio.py
@@ -2,11 +2,11 @@ __all__ = ["ArduinoGpio"]
 
 import asyncio
 from typing import Dict, Any, List
+import pyfirmata
+from yaqd_core import Base
 
-from yaqd_core import Hardware
 
-
-class ArduinoGpio(Hardware):
+class ArduinoGpio(Base):
     _kind = "arduino-gpio"
     traits: List[str] = []
     defaults: Dict[str, Any] = {}
@@ -14,7 +14,14 @@ class ArduinoGpio(Hardware):
     def __init__(self, name, config, config_filepath):
         super().__init__(name, config, config_filepath)
         # Perform any unique initialization
-
+        self.pinNumber = config["index"]
+        self.mode = config["mode"]
+        
+        if self.mode == 'digital':
+            self.board = pyfirmata.digital[self.pinNumber]
+        elif self.mode == 'analog':
+            self.board = pyfirmata.analog[self.pinNumber]
+        self.value = self.board.read()
 
     def _load_state(self, state):
         """Load an initial state from a dictionary (typically read from the state.toml file).
@@ -36,12 +43,19 @@ class ArduinoGpio(Hardware):
         state["value"] = self.value
         return state
 
-
-
-
-    def _set_position(self, position):
-        ...
-
+    def set_position(self, position):
+        if self.mode == "digital":
+            assert position == 1 or position == 0
+        elif self.mode == "analog":
+            assert position <= 5
+        self.board.write(position)
+        self.value = self.board.read()
+    
+    def get_position(self):
+        return self.value
+    
+    def get_mode(self):
+        return self.mode
 
     async def update_state(self):
         """Continually monitor and update the current daemon state."""
@@ -53,4 +67,7 @@ class ArduinoGpio(Hardware):
             # This one waits for something to trigger the "busy" state
             # (Setting `self._busy = True)
             # Otherwise, you can simply `await asyncio.sleep(0.01)`
-            await self._busy_sig.wait()
+            if self.mode == 'analog':
+                await self._busy_sig.wait()
+            else:
+                await asyncio.sleep(0.01)
